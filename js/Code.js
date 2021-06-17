ddc2a20642cc35f2352d06afdb446f39c7247031
AFRIN <74140658+AfrinSAnish@users.noreply.github.com>
2021-06-17 20:34:25 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,27 @@
+const accessCode1 = "RASIKA";
+const accessCode2 = "RANCHODDAS SHAMALDAS CHANCHAD";
+const accessCode3 = "SIMRAN";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("HINT: AKRISA", 100,50)
+    fill("lightblue")
+    text("Name of Kareena Kapoor in Bajrangi Bhaijaan", 100,70)
+// add code for displaying rest of the names and the hints.
+
+   fill("white")
+   textSize(15)
+   text("RIAMRN", 100,270)
+   fill("lightblue")
+   text("What is the name of the lead female character in \n Dilwale Dulhania Le Jayenge?",100,300)
+    
+
+   fill("white")
+   textSize(15)
+   text("DDOHNCARAS SDLAHMAAS CDHANHAC", 680,120)
+   fill("lightblue")
+   text("In 3 Idiots, what is Rancho's full name(Include Space)?",620,150)
+
+}
